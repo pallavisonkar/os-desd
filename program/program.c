a2066b6615fabeef633f521e2044c27e8d2c85aa
Pallavi Sonkar <pallavisonkar9@gmail.com>
2020-10-21 22:02:16 +0530

    Demo of program vs process

---
+++ program/program.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int add(int a, int b)
+{
+    int sum = 0;
+    sum = a+b;
+    return sum;
+}
+
+int main(int argc, char const *argv[])
+{
+    printf("program #1\n");
+    printf("Addition: %d\n",add(10,20));
+    return 0;
+}
+
+    
