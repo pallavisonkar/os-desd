5b331e409fe5e3be8582ba2fe4b65f09c3945b3a
Pallavi Sonkar <pallavisonkar9@gmail.com>
2020-10-26 19:10:49 +0530

    Fairness policy of demo

--- thread/threaddemo.c
+++ thread/threaddemo.c
@@ -11,10 +11,7 @@ int main(int argc, char const *argv[])
     pthread_t tid;
     printf("Before thread creation\n");
     pthread_create(&tid, NULL, thread, NULL);
-    printf("After thread creation\n");
-    sleep(1);
-    printf("After sleep creation\n");
-    
+              
     
     return 0;
 } 
